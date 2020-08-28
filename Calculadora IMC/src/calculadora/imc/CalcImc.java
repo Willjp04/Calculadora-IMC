8bd3132a78cb83a02dcab76fd61e8e89ac7674dc
Willian Sostenes <sostenes655@gmail.com>
2020-08-28 11:40:25 -0300

    Finalização Calculadora IMC

--- Calculadora IMC/src/calculadora/imc/CalcImc.java
+++ Calculadora IMC/src/calculadora/imc/CalcImc.java
@@ -42,6 +42,8 @@ public class CalcImc extends javax.swing.JFrame {
         jtfImc = new javax.swing.JTextField();
         jbuCalcular = new javax.swing.JButton();
         jbuLimpar = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jlaResultado = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -77,6 +79,30 @@ public class CalcImc extends javax.swing.JFrame {
         jbuLimpar.setFont(new java.awt.Font("Tahoma", 3, 14)); // NOI18N
         jbuLimpar.setForeground(new java.awt.Color(0, 153, 255));
         jbuLimpar.setText("Limpar");
+        jbuLimpar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbuLimparActionPerformed(evt);
+            }
+        });
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Resultado", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 3, 14), new java.awt.Color(0, 51, 255))); // NOI18N
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jlaResultado, javax.swing.GroupLayout.DEFAULT_SIZE, 166, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jlaResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(22, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -99,12 +125,15 @@ public class CalcImc extends javax.swing.JFrame {
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jlaImc)
                                 .addGap(20, 20, 20)
-                                .addComponent(jtfImc, javax.swing.GroupLayout.PREFERRED_SIZE, 69, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(jtfImc, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jbuCalcular)
                                 .addGap(33, 33, 33)
                                 .addComponent(jbuLimpar)))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -125,7 +154,9 @@ public class CalcImc extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jlaImc)
                     .addComponent(jtfImc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(85, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -151,9 +182,36 @@ altura = Float.valueOf(jtfAltura.getText());
 peso = Float.valueOf(jtfPeso.getText());
 imc = peso/(altura*altura);
 jtfImc.setText(String.valueOf(imc));
-
+jlaResultado.setText(VerificaImc(imc));
     }//GEN-LAST:event_jbuCalcularActionPerformed
 
+    private void jbuLimparActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbuLimparActionPerformed
+     jtfAltura.setText("");
+     jtfPeso.setText("");
+     jtfImc.setText("");
+    }//GEN-LAST:event_jbuLimparActionPerformed
+public String VerificaImc(Float imc){
+  String Resultado = null;
+    if(imc <=18.5)
+        Resultado = "Abaixo do Peso";
+    else if(imc >18.5 && imc <=24.99)
+        Resultado = "Peso Normal";
+    else if(imc >=25.0 && imc<=29.99)
+        Resultado = "Sobrepeso";
+    else if(imc >=30.0 && imc<=34.99)
+        Resultado ="Obesidade Grau I";
+    else if(imc >=35.0 && imc<=39.99)
+        Resultado = "Obesidade Grau II";
+    else if(imc >39.99)
+        Resultado ="Obesidade Grau III";
+    return Resultado;
+
+}
+
+                           
+
+              
+        
     /**
      * @param args the command line arguments
      */
@@ -191,11 +249,13 @@ jtfImc.setText(String.valueOf(imc));
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JButton jbuCalcular;
     private javax.swing.JButton jbuLimpar;
     private javax.swing.JLabel jlaAltura;
     private javax.swing.JLabel jlaImc;
     private javax.swing.JLabel jlaPeso;
+    private javax.swing.JLabel jlaResultado;
     private javax.swing.JTextField jtfAltura;
     private javax.swing.JTextField jtfImc;
     private javax.swing.JTextField jtfPeso;
